73bacfdf705956357b759f83e6999e5a05ef632b
Oleksii <alexmatvichuk@gmail.com>
2021-10-07 13:43:13 +0200

    up

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,18 @@ import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const contactsReducer = (state = [], { type, payload }) => {
+  const checkUserName = (state, payload) => {
+    if (state.some(item => item.name === payload.name)) {
+      alert(`${payload.name} is already in contacts`);
+      return state;
+    } else {
+      return [...state, payload];
+    }
+  };
+
   switch (type) {
     case 'app/addContacts':
-      return [...state, payload];
+      return checkUserName(state, payload);
 
     case 'contactsList / deleteUserContacts':
       return state.filter(item => item.id !== payload);
